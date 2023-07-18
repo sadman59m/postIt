b2be6ab68a67d999103e058a20397172c4a8754a
Sadman Sakib <mahmudshadman@gmail.com>
2023-07-19 03:55:50 +0600

    Delete post functionality

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -135,6 +135,30 @@ exports.updatePost = (req, res, next) => {
     });
 };
 
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Post not found.");
+        error.statusCode = 404;
+        throw error;
+      }
+      clearImage(post.imageUrl);
+      return Post.findByIdAndRemove(postId);
+    })
+    .then((resutl) => {
+      res.status(200).json({ message: "Post Deleted." });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+// Helper Fucntion
 const clearImage = (filePath) => {
   filePath = path.join(__dirname, "..", filePath);
   fs.unlink(filePath, (err) => {
--- server/routes/feed.js
+++ server/routes/feed.js
@@ -27,4 +27,6 @@ router.put(
   feedController.updatePost
 );
 
+router.delete("/post/:postId", feedController.deletePost);
+
 module.exports = router;
