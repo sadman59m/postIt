e9ee03649db97ed7437d4e0c751a79ebc443b15d
Sadman Sakib <mahmudshadman@gmail.com>
2023-07-19 01:48:05 +0600

    image file upload, FormData() and multer

--- server/app.js
+++ server/app.js
@@ -3,13 +3,38 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const multer = require("multer");
+// const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 const feedRoute = require("./routes/feed");
 
-app.use(bodyParser.json());
+const fileStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
 
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(bodyParser.json());
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -27,13 +27,20 @@ exports.createPost = (req, res, next) => {
     throw error;
   }
 
+  if (!req.file) {
+    const error = new Error("No image provided");
+    error.statusCode = 422;
+    throw error;
+  }
+
   const title = req.body.title;
   const content = req.body.content;
+  const imageUrl = req.file.path.replace("\\", "/");
 
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: "images/boat.png",
+    imageUrl: imageUrl,
     creator: {
       name: "Sadman",
     },
