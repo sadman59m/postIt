f19acfbfb9fdb742f1525535eca19b03affe413a
Sadman Sakib <mahmudshadman@gmail.com>
2023-07-19 03:37:08 +0600

    edit post, clearImage helper

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -10,12 +10,21 @@ router.get("/posts", feedController.getPosts);
 router.post(
   "/post",
   [
-    body("title").trim().isLength({ min: 7 }),
+    body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
   ],
   feedController.createPost
 );
 
 router.get("/post/:postId", feedController.getPost);
+
+router.put(
+  "/post/:postId",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.updatePost
+);
 
 module.exports = router;
