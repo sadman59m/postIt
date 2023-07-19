ed76c8e6eceb51403f767b7d05c190701e0105a1
Sadman Sakib <mahmudshadman@gmail.com>
2023-07-19 18:53:38 +0600

    using middleware to validate jwt token

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -4,17 +4,14 @@ const { body } = require("express-validator");
 const router = express.Router();
 
 const feedController = require("../controllers/feed");
+const routeCheck = require("../middleware/is_auth");
 
-router.get("/posts", feedController.getPosts);
+router.get("/posts", routeCheck.isAuth, feedController.getPosts);
 
-router.post(
-  "/post",
-  [
-    body("title").trim().isLength({ min: 5 }),
-    body("content").trim().isLength({ min: 5 }),
-  ],
-  feedController.createPost
-);
+router.post("/post", [
+  body("title").trim().isLength({ min: 5 }),
+  body("content").trim().isLength({ min: 5 }),
+]);
 
 router.get("/post/:postId", feedController.getPost);
 
