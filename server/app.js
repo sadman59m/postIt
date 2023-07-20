1742ababd0926554ad51d0419ee0253b8c2be102
Sadman Sakib <mahmudshadman@gmail.com>
2023-07-21 02:57:29 +0600

    creating socket.io instance, sharing accross files

--- server/app.js
+++ server/app.js
@@ -69,12 +69,7 @@ mongoose
   .then((result) => {
     // console.log("connected");
     const server = app.listen(8080);
-    const io = require("socket.io")(server, {
-      cors: {
-        origin: "http://localhost:3000",
-        // methods: ["GET", "POST", "PUT", "DELETE"],
-      },
-    });
+    const io = require("./socket").init(server);
     io.on("connection", (socket) => {
       console.log("client connected.");
     });
