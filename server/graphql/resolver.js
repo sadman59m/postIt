fbf810cd125be74c09f1e86ceb6c0777c8b7a415
Sadman Sakib <mahmudshadman@gmail.com>
2023-07-22 19:53:30 +0600

    adding input validation

--- server/graphql/resolver.js
+++ server/graphql/resolver.js
@@ -1,8 +1,24 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const validator = require("validator");
 
 module.exports = {
   createUser: async function ({ userInput }, req) {
+    // try {
+    const errors = [];
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: "Invalid Email." });
+    }
+    if (
+      validator.isEmpty(userInput.password) ||
+      !validator.isLength(userInput.password, { min: 5 })
+    ) {
+      errors.push({ message: "Invalid password" });
+    }
+    if (errors.length > 0) {
+      const error = new Error("Invalid entries");
+      throw error;
+    }
     const existingUser = await User.findOne({ email: userInput.email });
     if (existingUser) {
       const error = new Error("User already exists.");
@@ -16,5 +32,8 @@ module.exports = {
     });
     const createdUser = await user.save();
     return { ...createdUser._doc, _id: createdUser._id.toString() };
+    // } catch (err) {
+    //   console.log(err);
+    // }
   },
 };
